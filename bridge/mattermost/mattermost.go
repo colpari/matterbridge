d5c630e97ac0908d3b2f73cc71ff5efb03a8c7cd
Artur Dunskyy <artur.dunskyy@colpari.cx>
2023-07-03 16:28:19 +0200

    Insert the user ID for mention in  slack

--- bridge/mattermost/mattermost.go
+++ bridge/mattermost/mattermost.go
@@ -189,7 +189,7 @@ func (b *Bmattermost) Send(msg config.Message) (string, error) {
 
 	// Prepend nick if configured
 	if b.GetBool("PrefixMessagesWithNick") {
-		msg.Text = msg.Username + msg.Text
+		msg.Text = "[ " + msg.Username + " | " + "<@" + msg.UserID + ">" + " ]: " + msg.Text
 	}
 
 	// Edit message if we have an ID
